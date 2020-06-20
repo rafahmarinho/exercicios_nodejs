f2b6c1719e68ceddd8a7ac006e3c278f1752d62f
rafahmarinho <rafael.marinho96@hotmail.com>
2020-06-19 21:21:22 -0300

    3 atividade completa

--- atividade_3/index.js
+++ atividade_3/index.js
@@ -1,5 +1,11 @@
 // ## Importe a biblioteca ##
+let rs = require ("readline-sync")
 
+let lado = rs.question (console.log('Informe o tamanho em cm de um dos lados do seu quadrado?' ))
 
+let area = lado * lado
 
+console.log('====================================================================')
+
+console.log ('A área do seu quadrado é ' + area + ' cm.')
 // ## Faça o código ##
